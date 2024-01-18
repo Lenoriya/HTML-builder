df9885f9261e5683175667a5bfc73881a308c40e
Helen Kruminia <lena_kru@tut.by>
2024-01-18 12:46:36 +0300

    feat: 01-read-file is complete

---
+++ 01-read-file/index.js
@@ -0,0 +1,14 @@
+#!javascript
+// Load the fs (filesystem) module.
+var fs = require('fs');
+// Read the contents of the file into memory.
+fs.readFile('text.txt', function (err, logData) {
+// If an error occurred, throwing it will
+  // display the exception and kill our app.
+  if (err) throw err;
+// logData is a Buffer, convert to string.
+  var text = logData.toString();
+
+console.log(text);
+
+});
